79f45f570c8b165f49fdae217007c0bff3bedfbf
ghostbuster91 <kghost0@gmail.com>
2020-09-21 20:14:33 +0200

    Migrate to 0.7.3

--- server/src/main/scala/Main.scala
+++ server/src/main/scala/Main.scala
@@ -1,12 +1,13 @@
-import cats.effect.IO
+import cats.effect.{ExitCode, IO, IOApp}
 import com.example.protos.hello._
 import fs2._
 import io.grpc._
 import io.grpc.protobuf.services.ProtoReflectionService
 import org.lyranthe.fs2_grpc.java_runtime.implicits._
+
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class ExampleImplementation extends GreeterFs2Grpc[IO] {
+class ExampleImplementation extends GreeterFs2Grpc[IO, Metadata] {
   override def sayHello(request: HelloRequest,
                         clientHeaders: Metadata): IO[HelloReply] = {
     IO(HelloReply("Request name is: " + request.name))
@@ -19,16 +20,19 @@ class ExampleImplementation extends GreeterFs2Grpc[IO] {
   }
 }
 
-object Main extends StreamApp[IO] {
+object Main extends IOApp {
   val helloService: ServerServiceDefinition =
     GreeterFs2Grpc.bindService(new ExampleImplementation)
-  def stream(args: List[String], requestShutdown: IO[Unit]): fs2.Stream[IO, StreamApp.ExitCode] = {
+
+  override def run(args: List[String]): IO[ExitCode] = {
     ServerBuilder
       .forPort(9999)
       .addService(helloService)
-      .addService(ProtoReflectionService.newInstance())
       .stream[IO]
       .evalMap(server => IO(server.start()))
       .evalMap(_ => IO.never)
+      .compile
+      .drain
+      .as(ExitCode.Success)
   }
 }
